a60d955e47beca7e0a01172ba94fc0ba97588e36
crenaz <crenaz@gmail.com>
2016-05-01 01:09:29 -0400

    Update test.js

--- controllers/test.js
+++ controllers/test.js
@@ -1,1 +1,11 @@
-...
+var express = require('express');
+var router = require('express').Router();
+var path = require('path');
+
+router.use(express.static(__dirname + '/../assets'));
+
+router.get('/test', function (req, res) {
+    res.sendFile('test.html', { root: path.join(__dirname, '../views') });
+});
+
+module.exports = router;
